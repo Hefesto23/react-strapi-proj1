99d85e528f950c91317faf5f92d103a45bccbe94
Hefesto23 <vini.ra@outlook.com>
2023-06-25 11:54:02 +0100

    configuring styled components

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import Home from './pages/Home/Home';
+import ReactDOM from 'react-dom';
+import { GlobalStyles } from './styles/global-styles';
+import Home from './templates/App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <Home />
+    <GlobalStyles />
   </React.StrictMode>,
+  document.getElementById('root'),
 );
